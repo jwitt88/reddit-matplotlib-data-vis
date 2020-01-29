5f1b6d981d378d5aef10c8cb205dae99e95fafc0
Clyde W <59844570+jwitt88@users.noreply.github.com>
2020-01-29 01:34:00 -0600

    Updates to main_single.py
    - Added the display of 'comments per day'.
    - Updated display to only show the ten most recent posts until I come up with a better way to display data.

--- main_single.py
+++ main_single.py
@@ -13,30 +13,41 @@ keyword = str(filename.split('-')[1]).replace('.csv', '')
 #read it into a data frame
 df = pd.read_csv(filename)
 
-#convert the desired values into lists
-labels = df['Post ID'].tolist()
-score_vals = df[' Score'].tolist()
-com_vals = df[' Comments'].tolist()
+#days since posting
+now = pd.to_datetime('today')
+df['Date'] = pd.to_datetime(df['Date'])
+df['Date_Difference'] = (now - df['Date']).dt.days
 
+#convert the desired values into lists (only show 10)
+labels = df['Post ID'][:10].tolist()
+score_vals = df['Score'][:10].tolist()
+com_vals = df['Comments'][:10].tolist()
+
+#calculate the number of comments per day
+df['Comments_Per_Day'] = df['Comments'] / df['Date_Difference']
+coms_per_day = df['Comments_Per_Day'][:10].tolist()
+
+print(df['Comments_Per_Day'])
 
 #plot details, mostl sourced from:
 #https://matplotlib.org/gallery/lines_bars_and_markers/barchart.html#sphx-glr-gallery-lines-bars-and-markers-barchart-py
 
 plt.style.use('seaborn')
 x = np.arange(len(labels))  # the label locations
-width = 0.35  # the width of the bars
+width = 0.30  # the width of the bars
 
 fig, ax = plt.subplots()
 
-rects1 = ax.bar(x - width/2, score_vals, width, label='Post Score')
-rects2 = ax.bar(x + width/2, com_vals, width, label='Number of Comments')
+rects1 = ax.bar(x + .10, score_vals, width, label='Post Score')
+rects2 = ax.bar(x + .30, com_vals, width, label='Number of Comments')
+rects3 = ax.bar(x + .50, coms_per_day, width, label='Comments Per Day')
 
 
 ax.set_title(f'r/{sub} posts containing \'{keyword}\'')
 
 ax.set_xlabel('Post ID')
 ax.set_xticks(x)
-ax.set_xticklabels(labels)
+ax.set_xticklabels(labels, rotation='vertical')
 ax.set_ylabel('Values')
 
 ax.legend()
