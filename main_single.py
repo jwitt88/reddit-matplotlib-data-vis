7629bb1ad9e762bf63bfa5f676aea2f1a018600e
Clyde W <59844570+jwitt88@users.noreply.github.com>
2020-01-30 15:21:40 -0600

    Overhaul to main_single.py
    - Reworked main_single.py to identify keywords present in the data file, and allow the user to specify which results are to be plotted/summarized.

--- main_single.py
+++ main_single.py
@@ -3,6 +3,12 @@ import pandas as pd
 import numpy as np
 from sys import argv
 
+"""
+TODO:
+Rework to avoid this warning.
+"""
+pd.options.mode.chained_assignment = None
+
 #pass the name of the file for analysis
 script, filename = argv
 
@@ -12,68 +18,110 @@ sub = str(filename.split('-')[0])
 #read it into a data frame
 meta_df = pd.read_csv(filename)
 
-#count the number of unique keywords found in the file
+#count the number of unique list_of_keywords found in the file
 keyword_count = len(meta_df['Keyword'].unique())
 
 #make a list of the unique values
-keyword = meta_df['Keyword'].unique()
+list_of_keywords = meta_df['Keyword'].unique()
 
-
-#display the unique values, and ask the user which ones they want to compare
-print(f"""
-    (!) Data available for {keyword_count} unique search term(s).
-        Please select from the below options: \n""")
-
-for count, item in enumerate(keyword):
-    print(f"\tOption {count} - {item}")
-
-sel1 = int(input("\n > Input the option number of your first selection: "))
-sel2 = int(input(" > Input the option number of your second selection: "))
-
-#split the results into two distinct data frames
-sel1_df = meta_df[meta_df['Keyword'] == keyword[sel1]]
-sel2_df = meta_df[meta_df['Keyword'] == keyword[sel2]]
-
-
-
-
-#reworking this tomorrow
 """
+TODO:
+Put the below in a try loop, and re-factor.
+"""
+print(f"\n   (!) Successfully loaded {filename}")
+print(f"\n     - Data available for {keyword_count} unique result sets:")
+print(f"       {list_of_keywords}\n")
+
+choice_count = int(input(" > How many sets would you like to compare? "))
+choice_list = []
+
+
+#if the user requests the total number of variables:
+if choice_count >= len(meta_df['Keyword'].unique()):
+
+    #assign every available value to the choice list
+    for item in list_of_keywords:
+        choice_list.append(item)
+
+#otherwise, we're going to ask the user choose what to include
+else:
+
+    #make a dictionary of all the possible list_of_keywords
+    keyword_dict = {key: value for key, value in enumerate(list_of_keywords)}
+
+    #display the options
+    print(f"\n   Please select from the below result sets: \n")
+    for key in keyword_dict:
+        print(f"\t{key} - {keyword_dict[key]}")
+
+    #add selections until the user has reached the chosen amount
+    """
+    TODO:
+    Eventually avoid duplicates...
+    """
+    print()
+    for i in range(choice_count):
+        selection = int(input(" > Add a selection: "))
+        choice_list.append(keyword_dict[selection])
+
+
+#assign a df for every keyword to a dictionary
+meta_dict = {}
+for item in choice_list:
+    meta_dict[item] = meta_df[meta_df['Keyword'] == item]
+
+
+
 #days since posting
 now = pd.to_datetime('today')
-df['Date'] = pd.to_datetime(df['Date'])
-df['Date_Difference'] = (now - df['Date']).dt.days
-
-#convert the desired values into lists (only show 10)
-labels = df['Post ID'][:10].tolist()
-score_vals = df['Score'][:10].tolist()
-com_vals = df['Comments'][:10].tolist()
-
-#calculate the number of comments per day
-df['Comments_Per_Day'] = df['Comments'] / df['Date_Difference']
-coms_per_day = df['Comments_Per_Day'][:10].tolist()
 
 
-#plot details, mostl sourced from:
-#https://matplotlib.org/gallery/lines_bars_and_markers/barchart.html#sphx-glr-gallery-lines-bars-and-markers-barchart-py
+"""
+TODO:
+Refactor this into multiple sections...
+"""
+#outputting some results...
+for key in meta_dict:
 
-plt.style.use('seaborn')
-x = np.arange(len(labels))  # the label locations
-width = 0.30  # the width of the bars
+    #calculate the date and differential
+    meta_dict[key]['Date'] = pd.to_datetime(meta_dict[key]['Date'])
+    meta_dict[key]['Date_Difference'] = (now - meta_dict[key]['Date']).dt.days
 
-fig, ax = plt.subplots()
+    knum_of_posts = meta_dict[key]['Post_ID'].count()
 
-rects1 = ax.bar(x + .10, score_vals, width, label='Post Score')
-rects2 = ax.bar(x + .30, com_vals, width, label='Number of Comments')
-rects3 = ax.bar(x + .50, coms_per_day, width, label='Comments Per Day')
+    kavg_age = meta_dict[key]['Date_Difference'].mean()
+    koldest = meta_dict[key]['Date'].min()
 
-ax.set_title(f'Posts retrieved from r/{sub}')
+    kmax_score = meta_dict[key]['Score'].max()
+    kavg_score = meta_dict[key]['Score'].mean()
 
-ax.set_xlabel('Post ID')
-ax.set_xticks(x)
-ax.set_xticklabels(labels, rotation='vertical')
-ax.set_ylabel('Values')
+    kmax_comments = meta_dict[key]['Comments'].max()
+    kavg_comments = meta_dict[key]['Comments'].mean()
 
-ax.legend()
-fig.tight_layout()
-plt.show()"""
+    print(f"\n *** SUMMARY OF POST(S) CONTAINING '{key}' ***\n")
+
+    print(f"   - Post count: {knum_of_posts}")
+    print(f"   - Oldest post: {str(koldest)[:10]}")
+    print(f"   - Avg post age: ~{int(kavg_age)} day(s)\n")
+
+    print(f"   - Max score: {kmax_score}")
+    print(f"   - Avg score: {kavg_score}\n")
+
+    print(f"   - Max comments: {kmax_comments}")
+    print(f"   - Avg # of comments: {kavg_comments}\n")
+
+    """
+    MATPLOTLIB START
+    """
+    plt.style.use('seaborn')
+    fig, ax = plt.subplots(figsize=(4, 4))
+
+    fig.canvas.set_window_title(key)
+
+    x_val = ['# of Posts', 'Avg. Score', 'Avg. Comments']
+    y_val = [knum_of_posts, kavg_score, kavg_comments]
+
+    plt.tight_layout()
+    ax.bar(x_val, y_val)
+
+plt.show()
