473433a93e1ab1b8f08b473940192a8de101069f
Clyde W <59844570+jwitt88@users.noreply.github.com>
2020-01-30 01:50:31 -0600

    Minor updates to main_single.py
    - Removed unecessary keyword representations in the output.
    - Removed a print line containing 'comments per day' values.

--- main_single.py
+++ main_single.py
@@ -8,7 +8,6 @@ script, filename = argv
 
 #get the sub/key from the fiename
 sub = str(filename.split('-')[0])
-keyword = str(filename.split('-')[1]).replace('.csv', '')
 
 #read it into a data frame
 df = pd.read_csv(filename)
@@ -27,7 +26,6 @@ com_vals = df['Comments'][:10].tolist()
 df['Comments_Per_Day'] = df['Comments'] / df['Date_Difference']
 coms_per_day = df['Comments_Per_Day'][:10].tolist()
 
-print(df['Comments_Per_Day'])
 
 #plot details, mostl sourced from:
 #https://matplotlib.org/gallery/lines_bars_and_markers/barchart.html#sphx-glr-gallery-lines-bars-and-markers-barchart-py
@@ -42,8 +40,7 @@ rects1 = ax.bar(x + .10, score_vals, width, label='Post Score')
 rects2 = ax.bar(x + .30, com_vals, width, label='Number of Comments')
 rects3 = ax.bar(x + .50, coms_per_day, width, label='Comments Per Day')
 
-
-ax.set_title(f'r/{sub} posts containing \'{keyword}\'')
+ax.set_title(f'Posts retrieved from r/{sub}')
 
 ax.set_xlabel('Post ID')
 ax.set_xticks(x)
